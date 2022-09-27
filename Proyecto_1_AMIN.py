5ce4d9b4b92b9e73d909846debaa4be027ec9853
MatiPino23 <mpino@ing.ucsc.cl>
2022-09-27 01:01:36 -0400

    Fitness

--- Proyecto_1_AMIN.py
+++ Proyecto_1_AMIN.py
@@ -2,6 +2,19 @@ import numpy as np
 import time
 import sys
 
+def fitness(tablero):
+    rows, columns= tablero.shape
+    count = 0
+    list = []
+    for tab in tablero:
+        for i in range(columns-1):
+            for j in range(i+1,columns):
+                if abs(tab[i]-tab[j]) == abs(i-j):
+                    count += 1
+        list.append(count)
+        count = 0
+    return list
+
 Comenzar= time.time()
 
 if len(sys.argv)==7:
@@ -19,11 +32,14 @@ np.random.seed(semilla)
 
 poblacion= np.zeros([T_poblacion ,T_tablero], dtype=int)  
 for i in range(T_poblacion):
-    poblacion[i] = range(0,T_tablero)
+    poblacion[i] = np.arange(0,T_tablero)
     np.random.shuffle(poblacion[i])
 
 print(poblacion)
 
+fit=fitness(poblacion)
+
+print(fit)
 
 
 
